f620b003ccd139eda5a492677200ff50d6eb5f0e
Jukelyn <viperkillergaming@gmail.com>
2024-07-09 11:26:42 -0400

    Added some docstrings and comments

--- src/copycat.py
+++ src/copycat.py
@@ -1,3 +1,4 @@
+"""Sys provides functionality to exit the program. tkinter provides UI."""
 import sys
 import tkinter as tk
 from tkinter import scrolledtext
@@ -6,15 +7,22 @@ EXIT_KEYWORDS = ["exit", "quit", "bye"]
 
 
 def get_response(user_in):
+    '''
+    Gets the user's response and just copies it back. This is only a function
+    because it may get updated to respond differently in the future. For now
+    it may seem a bit redundant or useless.
+    '''
     return f"You said '{user_in}'"
 
 
-def show_response(event=None):
-    # Function to display the CopyCat response in the GUI
+def show_response(event=None):  # pylint: disable=unused-argument
+    '''
+    Displays the CopyCat response in the GUI alongside the user input
+    '''
 
     user_in = input_box.get("1.0", tk.END).strip()
     input_box.delete("1.0", tk.END)
-    log.config(state=tk.NORMAL)
+    log.config(state=tk.NORMAL)  # DISABLED -> NORMAL to allow tk to show stuff
 
     if user_in.lower() in EXIT_KEYWORDS:
         log.insert(tk.END, "CopyCat: Goodbye!\n")
@@ -29,30 +37,30 @@ def show_response(event=None):
     log.config(state=tk.DISABLED)
 
 
-# Main GUI window
+# Main window
 window = tk.Tk()
 window.title("Stop copying me")
 
-# Chat log
+# The chat log
 log = scrolledtext.ScrolledText(window, width=60, height=20, wrap=tk.WORD)
 log.grid(row=0, column=0, columnspan=2, padx=10, pady=10)
 
 exit_words = EXIT_KEYWORDS
 welcome_message = f'Welcome! My name is CopyCat. \
-Enter any of the following to quit: {exit_words}.\n'
+Enter any of the following to quit: {exit_words}.\n'  # May edit later...
 
 log.insert(tk.END, welcome_message)
-log.config(state=tk.DISABLED)
+log.config(state=tk.DISABLED)  # Do not want user to be able to type in log
 
-# User input box
+# User input box (below the log)
 input_box = scrolledtext.ScrolledText(
     window, width=40, height=4, wrap=tk.WORD)
 input_box.grid(row=1, column=0, padx=10, pady=10)
 
-# Send button
+# Send button (beside the user input box)
 send = tk.Button(window, text="Send", command=show_response)
 send.grid(row=1, column=1, padx=10, pady=10)
 input_box.bind("<Return>", show_response)
 
-# Start the GUI event loop
+# Start the UI event loop
 window.mainloop()
